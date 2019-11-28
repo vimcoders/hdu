ae62f1b0aa4fb208cb0e396697cae150a43961b6
lujianbo <lujianbo@txnebula.com>
2019-11-28 22:14:30 +0800

    hdu 3791

---
+++ 3791.cpp
@@ -0,0 +1,66 @@
+#include<cstdio>
+
+using namespace std;
+
+class BinarySearchNode
+{
+	public:
+		BinarySearchNode(int v);
+		~BinarySearchNode();
+		BinarySearchNode *Left;
+		BinarySearchNode *Right;
+		int Value;
+};
+
+BinarySearchNode::BinarySearchNode(int v) 
+{
+	Value = v;
+}
+
+BinarySearchNode::~BinarySearchNode()
+{
+
+}
+
+class BinarySearchTree 
+{
+	public:
+		void Insert(int v);
+	private:
+		BinarySearchNode *root;
+};
+
+void BinarySearchTree::Insert(int v)
+{
+	BinarySearchNode *node = this->root;
+	BinarySearchNode newNode(v);
+
+	while (node != NULL) 
+	{
+		if (v <= node->Value) 
+		{
+			if (node->Left == NULL)
+			{
+				node = node->Left;
+				continue;
+			}
+
+			node->Left = &newNode;
+			return;
+		}
+
+		if (node->Right != NULL) 
+		{
+			node = node->Right;
+			continue;
+		}
+
+		node->Right = &newNode;
+		return;
+	}
+}
+
+int main(void) 
+{
+	printf("hello");
+}
